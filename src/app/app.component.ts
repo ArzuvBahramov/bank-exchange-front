2a54e211b3f5c7addee463b3ca534f9127137a61
Arzuv Bahramov <arzuv.bahramov@innowise.com>
2024-05-09 01:47:51 +0300

    implemented footer username

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "./model/user.model";
+import {AuthDataService} from "./services/data/auth/auth-data.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'bank-front';
+export class AppComponent implements OnInit{
+  user: User;
+
+  constructor(private authService: AuthDataService) {
+    this.user = {
+      id: 0,
+      firstname: '',
+      lastname: '',
+      username: '',
+      email: ''
+    }
+  }
+
+  loadUserDetails() {
+    this.authService.getDetails().subscribe({
+      next: (response) => {
+        this.user = response;
+      },
+      error: (err) => {
+        console.log(err);
+        throw (err);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadUserDetails();
+  }
 }
